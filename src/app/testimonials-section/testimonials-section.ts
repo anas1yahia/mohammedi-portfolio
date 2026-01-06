54a3f52bfb32f3f9579bc4341b04d21ecbfd58a7
Yahia Anas <anas.yahya42@gmail.com>
2026-01-06 22:16:20 +0100

    feat: add testimonials section with HTML, CSS, and TypeScript implementation

---
+++ src/app/testimonials-section/testimonials-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './testimonials-section.html',
+  styleUrl: './testimonials-section.scss'
+})
+export class TestimonialsSectionComponent {}
